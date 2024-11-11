461ac699d791dd70fa26c770073d2ecc04f4b1ee
Julia Daniluski <julia.o.daniluski@gmail.com>
2024-11-11 10:53:45 -0300

    acho que foi

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Crud Simples</title>
 </head>
 <body>
@@ -14,6 +15,8 @@
         <input type="email" name="email" required>
         <label>Senha:</label>
         <input type="password" name="senha" required>
+        <br>
+        <br>
         <input type="submit" value="Adicionar Usuário">
     </form>
     <hr>
